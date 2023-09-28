889c83dccf696007953a143d1a23abf34b649476
Tim Cross <theophilusx@gmail.com>
2023-09-28 10:58:40 +1000

    Remove unnecessary require

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -1,7 +1,6 @@
 (ns user
   (:require [integrant.core :as ig]
             [taoensso.timbre :as log]
-            [taoensso.timbre :refer [debug]]
             [theophilusx.auth.core :refer [read-config config system]]
             [theophilusx.auth.utils :refer [read-env]]
             [integrant.repl :refer [go halt reset]]
@@ -15,26 +14,22 @@
                               (reset! config cfg))))
 
 (defn start-system []
-  (debug "Starting system")
+  (log/debug "Starting system")
   (go)
   (reset! system rstate/system))
 
 (defn stop-system []
-  (debug "Halting system")
+  (log/debug "Halting system")
   (halt)
   (reset! system {}))
 
 (defn reset-system []
-  (debug "Resetting system")
+  (log/debug "Resetting system")
   (reset)
   (reset! system rstate/system))
 
 (comment
-  (defn my-play [v]
-    (condp re-matches v
-      #"^00.*$" {:status :match1 :value v}
-      #"^01.*$" {:status :match2 :value v}
-      #"^02.*$" {:status :match3 :value v}
-      {:status :generic-match :value v}))
-  (my-play "0400")
-  )
+  (start-system))
+
+
+
