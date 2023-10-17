580191248db051632f4c384bd0689761bef8ef64
Tim Cross <theophilusx@gmail.com>
2023-10-17 17:45:46 +1100

    Add minimum log levels to stop logging of some NS
    We don't want to do debug logging of core system libs, like Jety and Postgres etc

--- src/theophilusx/auth/log.clj
+++ src/theophilusx/auth/log.clj
@@ -1,12 +1,21 @@
 (ns theophilusx.auth.log
   (:require [integrant.core :as ig]
-            [taoensso.timbre :as log]
+            [taoensso.timbre :as tl]
             [taoensso.timbre.appenders.core :refer [spit-appender]]))
 
-(defmethod ig/init-key :theophilusx.auth.log/logger [_ {:keys [log-file]}]
-  (log/debug "Enabling logg9ing")
-  (log/merge-config! {:appenders {:spit (spit-appender {:fname log-file})}}))
+(defmethod ig/init-key :theophilusx.auth.log/logger [_ {:keys [log-file log-level]}]
+  (tl/debug "Enabling logg9ing")
+  (tl/merge-config! {:min-level [[#{"org.eclipse.jetty.*"
+                                    "com.mchange.*"} :error]
+                                 [#{"*"} log-level]]
+                     :appenders {:spit (spit-appender {:fname log-file})}}))
 
 ;; (defmethod ig/halt-key! :theophilusx.auth.log/logger [_ _]
 ;;   (log/debug "Disabling logging")
-;;   (log/merge-config! {:appenders {:spit {:enabled? false}}}))
+;;   (log/merge-config! {:appenders {:spit {:enabled? false}}})create)
+
+(defn record-problem [fn-name fn-call error & {:keys [data] :or {data ""}}]
+  (error (str fn-name " after calling: " fn-call " error: " error " " data)))
+
+
+
