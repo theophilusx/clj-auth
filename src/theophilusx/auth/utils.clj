08414d2f4e2863bc22d2cb705f822e16598fae64
Tim Cross <theophilusx@gmail.com>
2023-11-20 08:30:17 +1100

    Add JWT sign/verify functionality

--- src/theophilusx/auth/utils.clj
+++ src/theophilusx/auth/utils.clj
@@ -2,8 +2,24 @@
   (:require [clojure.edn :as edn]
             [clojure.string :refer [join]]
             [taoensso.timbre :as log]
+            [buddy.core.keys :as bkeys]
             [buddy.hashers :as hashers]
+            [buddy.sign.jwt :as jwt]
             [integrant.core :as ig]))
+
+(def key-info (atom nil))
+
+(defmethod  ig/init-key :theophilusx.auth.utils/key
+  [_ {:keys [jwt-private-key-file jwt-public-key-file
+             jwt-passphrase jwt-key-alg] :as config}]
+  (log/debug "utils/keys: config " config)
+  (let [priv-key-data (if jwt-private-key-file
+                        (bkeys/private-key jwt-private-key-file jwt-passphrase))
+        pub-key-data (if jwt-public-key-file
+                       (bkeys/public-key jwt-public-key-file))]
+    (reset! key-info {:private-key priv-key-data
+                      :public-key pub-key-data
+                      :alg jwt-key-alg})))
 
 (defn read-env
   "Load .env.edn file of envrionment specific settings."
@@ -32,9 +48,14 @@
                             :pass (:smtp-password env-data)
                             :dev-address (:smtp-dev-address env-data)
                             :from-address (:smtp-from env-data)}
-                           (:theophilusx.auth.mail/post cfg))]
+                           (:theophilusx.auth.mail/post cfg))
+          utils-cfg {:jwt-private-key-file (:jwt-private-key-file env-data)
+                     :jwt-public-key-file (:jwt-public-key-file env-data)
+                     :jwt-key-alg (:jwt-key-alg env-data)
+                     :jwt-passphrase (:jwt-passphrase env-data)}]
       (merge cfg {:theophilusx.auth.db/data-source db-cfg
-                  :theophilusx.auth.mail/post mail-cfg}))
+                  :theophilusx.auth.mail/post mail-cfg
+                  :theophilusx.auth.utils/key utils-cfg}))
     (catch Exception e
       (log/error "read-config: " (.getMessage e))
       nil)))
@@ -61,6 +82,35 @@
   if the stored version of the password should be updated."
   [supplied-pwd recorded-pwd]
   (hashers/verify supplied-pwd recorded-pwd))
+
+(defn jwt-sign
+  ([payload]
+   (jwt-sign payload {}))
+  ([payload opts]
+   (try 
+     (jwt/sign payload (:private-key @key-info) (merge opts {:alg (:alg @key-info)}))
+     (catch Exception e
+       (log/error (str "jwt-sign: " (.getMessage e)))
+       nil))))
+
+(defn jwt-verify
+  ([token]
+   (jwt-verify token {}))
+  ([token opts]
+   (try
+     (log/debug "jwt-verify: token = " token)
+     (log/debug "jwt-verify: Opts = " opts)
+     {:claims (jwt/unsign token (or (:public-key @key-info)
+                                    (:private-key @key-info))
+                          (merge opts {:alg (:alg @key-info)}))
+      :status :ok}
+     (catch Exception e
+       (let [e-data (ex-data e)]
+         {:status :error
+          :error-msg (.getMessage e)
+          :error-name (:type e-data)
+          :error-code (:cause e-data)
+          :claims nil})))))
 
 (comment
   (let [e-data ]))
--- test/theophilusx/auth/utils_test.clj
+++ test/theophilusx/auth/utils_test.clj
@@ -1,13 +1,16 @@
 (ns theophilusx.auth.utils-test
   (:require [clojure.test :refer [deftest is testing]]
             [theophilusx.auth.utils :as sut]
-            [clojure.string :refer [blank?]]))
+            [clojure.string :refer [blank? starts-with?]]
+            [java-time.api :as jt]))
 
 (deftest read-env
   (let [env-file "resources/dev-env.edn"
         env-keys #{:db-user :db-password :db-name :db-host :db-port :db-password-file
                    :db-script-path :smtp-server :smtp-port :smtp-tls :smtp-user :smtp-password
-                   :smtp-from :smtp-dev-address :config-file}]
+                   :smtp-from :smtp-dev-address :config-file
+                   :jwt-private-key-file :jwt-public-key-file
+                   :jwt-key-alg :jwt-passphrase}]
     (testing "read envrionment settings file"
       (let [ev (sut/read-env env-file)]
         (is (map? ev))
@@ -23,7 +26,8 @@
                    :theophilusx.auth.routes/site
                    :theophilusx.auth.core/web-server
                    :theophilusx.auth.db/data-source
-                   :theophilusx.auth.mail/post}]
+                   :theophilusx.auth.mail/post
+                   :theophilusx.auth.utils/key}]
     (testing "Successfully read system config"
       (let [cfg (sut/read-config env-data)]
         (is (map? cfg))
@@ -61,3 +65,126 @@
       (let [hp (sut/hash-pwd p)
             vfy (sut/verify-pwd "A test password" hp)]
         (is (not (:valid vfy)))))))
+
+(deftest jwt-sign
+  (testing "Basic signing of payload"
+    (let [token (sut/jwt-sign {:payuload "This is the payload"})]
+      (is (not (nil? token)))
+      (is (string? token))
+      (is (= 410 (count token))))))
+
+(deftest jwt-verify
+  (testing "Validate basic token"
+    (let [token (sut/jwt-sign {:payload "This is the payload"})
+          claims (sut/jwt-verify token)]
+      (println (str "Claims: " claims))
+      (is (map? claims))
+      (is (contains? claims :claims)))))
+
+(deftest sing-verify
+  (testing "Sign and verify claims with expiration"
+    (let [validated (-> {:payload "Hello World" :exp (jt/plus (jt/instant) (jt/minutes 3))}
+                        (sut/jwt-sign)
+                        (sut/jwt-verify))]
+      (is (map? validated))
+      (is (= :ok (:status validated)))
+      (is (contains? (:claims validated) :payload))
+      (is (contains? (:claims validated) :exp))))
+  (testing "Fail verify with token past expieration"
+    (let [verified (-> {:payload "WOn't see me" :exp (jt/plus (jt/instant) (jt/minutes 2))}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:now (jt/plus (jt/instant) (jt/minutes 3))}))]
+      (println (str "verified: " verified))
+      (is (map? verified))
+      (is (= :error (:status verified)))
+      (is (nil? (:claims verified)))
+      (is (starts-with? (:error-msg verified) "Token is expired"))
+      (is (= :exp (:error-code verified)))
+      (is (= :validation (:error-name verified)))))
+  (testing "Claim with valid issuer"
+    (let [verified (-> {:payload "fred" :iss "example.com"}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:iss ["example.com"]}))]
+      (is (map? verified))
+      (is (= :ok (:status verified)))
+      (is (= (set (keys (:claims verified))) #{:payload :iss}))))
+  (testing "Reject claim wiht invalid issuer"
+    (let [verified (-> {:payload "fred" :iss "bad-place.com"}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:iss ["example.com" "auth.example.com"]}))]
+      (is (map? verified))
+      (is (= :error (:status verified)))
+      (is (nil? (:claims verified)))
+      (is (starts-with? (:error-msg verified) "Issuer does not match"))
+      (is (= :iss (:error-code verified)))
+      (is (= :validation (:error-name verified)))))
+  (testing "Verify valid audience"
+    (let [verified (-> {:payload "fred" :aud ["user" "admin"]}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:aud "admin"}))]
+      (is (map? verified))
+      (is (= :ok (:status verified)))))
+  (testing "Fail verify due to bad audience"
+    (let [verified (-> {:payload "fred" :aud "guest"}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:aud "admin"}))]
+      (is (map? verified))
+      (is (= :error (:status verified)))
+      (is (nil? (:clamis verified)))
+      (is (starts-with? (:error-msg verified) "Audience does not match"))
+      (is (= :aud (:error-code verified)))
+      (is (= :validation (:error-name verified)))))
+  (testing "Verify valid subject"
+    (let [verified (-> {:payload "fred" :sub "subject"}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:sub "subject"}))]
+      (is (map? verified))
+      (is (= :ok (:status verified)))))
+  (testing "Fail verify due to bad subject"
+    (let [verified (-> {:payload "fred" :sub "guest"}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:sub "admin"}))]
+      (is (map? verified))
+      (is (= :error (:status verified)))
+      (is (nil? (:clamis verified)))
+      (is (starts-with? (:error-msg verified) "The subject does not match"))
+      (is (= :sub (:error-code verified)))
+      (is (= :validation (:error-name verified)))))
+  (testing "Verify with not before time claim"
+    (let [verified (-> {:payload "fred" :nbf (jt/instant)}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:now (jt/plus (jt/instant) (jt/minutes 1))}))]
+      (is (map? verified))
+      (is (= :ok (:status verified)))))
+  (testing "Fail verify due to bad not before time"
+    (let [verified (-> {:payload "fred" :nbf (jt/instant)}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:now (jt/minus (jt/instant) (jt/minutes 1))}))]
+      (is (map? verified))
+      (is (= :error (:status verified)))
+      (is (nil? (:clamis verified)))
+      (is (starts-with? (:error-msg verified) "Token is not yet valid"))
+      (is (= :nbf (:error-code verified)))
+      (is (= :validation (:error-name verified)))))
+  (testing "Verify with issue at time claim"
+    (let [verified (-> {:payload "fred" :iat (jt/instant)}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:now (jt/instant) :max-age 120}))]
+      (is (map? verified))
+      (is (= :ok (:status verified)))))
+  (testing "Fail verify due to max-age being exceeded"
+    (let [verified (-> {:payload "fred" :iat (jt/instant)}
+                       (sut/jwt-sign)
+                       (sut/jwt-verify {:now (jt/plus (jt/instant) (jt/minutes 3))
+                                        :max-age 120}))]
+      (is (map? verified))
+      (is (= :error (:status verified)))
+      (is (nil? (:clamis verified)))
+      (is (starts-with? (:error-msg verified) "Token is older than max-age"))
+      (is (= :max-age (:error-code verified)))
+      (is (= :validation (:error-name verified))))))
+
+(comment
+(jt/local-date-time)
+(jt/plus (jt/local-date-time) (jt/seconds 2))
+)
