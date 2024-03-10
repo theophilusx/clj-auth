223f7b43f68363f24bc32980f3db98449d10e2c2
Tim Cross <theophilusx@gmail.com>
2024-03-10 11:39:58 +1100

    Add watchers for config.edn and config.local.edn

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -4,7 +4,15 @@
             [theophilusx.auth.core :refer [config system]]
             [theophilusx.auth.utils :refer [read-config]]
             [integrant.repl :refer [go halt reset]]
-            [integrant.repl.state :as rstate]))
+            [integrant.repl.state :as rstate]
+            [lambdaisland.launchpad.watcher :refer [watch!]]))
+
+(defn reload-config [opts]
+  (println opts)
+  (read-config))
+
+(watch! {(.getCanonicalPath (clojure.java.io/file "resources/config.local.edn")) reload-config
+         (.getCanonicalPath (clojure.java.io/file "resources/config.edn"))       reload-config})
 
 (integrant.repl/set-prep! (fn []
                             (let [cfg (read-config)]
@@ -13,26 +21,26 @@
                               (reset! config cfg))))
 
 (defn start-system []
-  (log/info "Starting system")
-  (go)
-  (reset! system rstate/system)) 
+(log/info "Starting system")
+(go)
+(reset! system rstate/system)) 
 
 (defn stop-system []
-  (log/info "Halting system")
-  (halt)
-  (reset! system {}))
+(log/info "Halting system")
+(halt)
+(reset! system {}))
 
 (defn reset-system []
-  (log/info "Resetting system")
-  (reset)
-  (reset! system rstate/system))
+(log/info "Resetting system")
+(reset)
+(reset! system rstate/system))
 
 (comment
-  (play {:body {:email "fred@home" :first-name "Fred" :last-name "Dagg"}})
-  (start-system)
-  (let [env-data (read-env "resources/dev-env.edn")
-        cfg (read-config env-data)]
-    (reset! config cfg)))
+(play {:body {:email "fred@home" :first-name "Fred" :last-name "Dagg"}})
+(start-system)
+(let [env-data (read-env "resources/dev-env.edn")
+      cfg      (read-config env-data)]
+  (reset! config cfg)))
 
 
 
