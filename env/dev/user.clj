dda407c8be31353174d70d26f61fbf29bd33edea
Tim Cross <theophilusx@gmail.com>
2024-03-23 14:49:23 +1100

    Refining tool chain config

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -5,7 +5,8 @@
             [theophilusx.auth.utils :refer [read-config]]
             [integrant.repl :refer [go halt reset]]
             [integrant.repl.state :as rstate]
-            [lambdaisland.launchpad.watcher :refer [watch!]]))
+            [lambdaisland.launchpad.watcher :refer [watch!]]
+            [kaocha.repl :as k]))
 
 (defn reload-config [opts]
   (println opts)
---
+++ test/theophilusx/auth/hooks.clj
@@ -0,0 +1,30 @@
+(ns theophilusx.auth.hooks
+  (:require  [integrant.core :as ig]
+             [theophilusx.auth.core :refer [config system start-system
+                                            stop-system]]
+             [theophilusx.auth.utils :refer [read-config]]
+             [theophilusx.auth.log :as log]
+             [kaocha.hierarchy :as hierarchy]))
+
+(def standalone (atom false))
+
+(defn init-system [cfg]
+  (log/set-min-level :error)
+  (when (not @config)
+    (let [system-cfg (merge (read-config)
+                            {:theophilusx.auth.core/web-server
+                             {:port  3001
+                              :join? false
+                              :site  {:key :theophilusx.auth.routes/site}}})]
+      (reset! config system-cfg)
+      (reset! system (start-system system-cfg))
+      (reset! standalone true)))
+  cfg)
+
+(defn close-system [rslt]
+  (stop-system @system)
+  (reset! system nil)
+  (when @standalone (reset! standalone false))
+  rslt)
+
+
